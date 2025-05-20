9797c18e60b8efec72b2283cacecff22bfaa8f10
KrithikBavanendiran <bvino1522@gmail.com>
2025-05-20 19:38:10 +0100

    Lesson 27

---
+++ Class vehicle.py
@@ -0,0 +1,6 @@
+class vehicle:
+    def __init__(self,max_speed, mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+mercedes= vehicle(180,18)
+print(mercedes.max_speed,mercedes.mileage)
